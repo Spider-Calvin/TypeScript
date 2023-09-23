7238803ef78d092ef6b7b3d0dd13179fbd0efe9a
Calvin Das <calvindas7@gmail.com>
2023-09-24 00:32:42 +0530

    interface in type script

--- dist/index.js
+++ dist/index.js
@@ -8,3 +8,9 @@ const obj1 = {
     height: 12,
     weight: 30
 };
+const obj2 = {
+    height: 23,
+    weight: 10,
+    firstname: 'Spider',
+    lastname: 'Calvin'
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-type objtype = {
+interface objtype {
     height: number,
     weight: number,
     gender?:string //we made the gender optional
@@ -13,4 +13,16 @@ const obj:objtype ={
 const obj1:objtype ={
     height:12,
     weight:30
+}
+
+interface Newobjtype extends objtype {
+    firstname:string,
+    lastname:string
+}
+
+const obj2:Newobjtype = {
+    height:23,
+    weight:10,
+    firstname:'Spider',
+    lastname:'Calvin'
 }
