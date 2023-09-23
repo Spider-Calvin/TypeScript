070d3f13aa9889087c7b46d3671de2060f24e6a3
Calvin Das <calvindas7@gmail.com>
2023-09-24 00:06:47 +0530

    function in ts

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,9 @@
 let surname;
 surname = 12;
 surname = 'calvin';
+const func = (n, m) => {
+    console.log(n, m);
+    return String(n);
+};
+func(2, 'calvin');
 alert(surname);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 let surname : string | number;
 surname = 12
-surname ='calvin'
+surname ='calvin';
 
+const func = (n:number,m:string)=>{
+ console.log(n,m);
+ return String(n);
+}
+
+func(2,'calvin');
 alert(surname);
