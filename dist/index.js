5bf872748749c86aa1fe26bab28cadc693fe4d50
Calvin Das <calvindas7@gmail.com>
2023-09-24 01:07:32 +0530

    read only in object and functions

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,11 @@
 const getData = (product) => {
     console.log(product);
 };
+const productOne = {
+    name: "Macbook",
+    stock: 46,
+    price: 999999,
+    photo: "samplephotourl",
+    id: "asdnasjdasjkdas",
+};
+getData(productOne);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-type GetDataType = (product:{
+type product = {
     name: string;
     stock: number;
     price: number;
     photo: string;
-}) => void;
+    readonly id : string;
+}
+
+type GetDataType = (product :product) => void;
 
 const getData: GetDataType = (product) => {
     console.log(product);
 };
     
+const productOne: product = { 
+    name: "Macbook",
+    stock: 46,
+    price: 999999,
+    photo: "samplephotourl",
+    id: "asdnasjdasjkdas",
+};
+
+getData(productOne);
