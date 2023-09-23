8f3c76af5ba6b6e0bfcdebdac2c8b9c4eed007de
Calvin Das <calvindas7@gmail.com>
2023-09-24 00:45:48 +0530

    same default in different way

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,7 @@ const func = (n, m, l = 10) => {
     return n * m * 1;
 };
 func(25, 23);
+const func2 = (n, m, l = 20) => {
+    return n * m * 1;
+};
+func2(25, 23);
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ const func: FuncType = (n, m, l=10) => {
 };
 
 func(25, 23);
+
+const func2 = (n: number, m: number, l: number = 20): number => {
+    return n * m * 1;
+};
+func2(25, 23);
