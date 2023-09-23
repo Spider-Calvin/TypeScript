ec1516ea2a31a67466c44c00475ab18ba9d0d418
Calvin Das <calvindas7@gmail.com>
2023-09-24 01:13:47 +0530

    theme mode explaination

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 "use strict";
 const mode = 'light';
+// it can have values of the only defined not other values or else it will give error
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 type thememode = 'light' | 'dark';
 
 const mode:thememode = 'light';
+// it can have values of the only defined not other values or else it will give error
