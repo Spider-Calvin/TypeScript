3a45feeb9d1ffd0db0c36d80ad0616aa6bafc5c9
Calvin Das <calvindas7@gmail.com>
2023-09-24 00:54:58 +0530

    small fix of previous

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function Lol(n:number): number{
     return 45
 }
 
-type FuncType = (...m: number[]) => number;
+type FuncType = (m: number) => number;
 
 const lol: FuncType = function lol(n){
     return n
