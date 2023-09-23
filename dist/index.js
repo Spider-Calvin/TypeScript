0f0f52af783d5e891193408a7d009b941e67f426
Calvin Das <calvindas7@gmail.com>
2023-09-24 01:12:45 +0530

    thememode property of ts

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,2 @@
 "use strict";
-const getData = (product) => {
-    console.log(product);
-};
-const productOne = {
-    name: "Macbook",
-    stock: 46,
-    price: 999999,
-    photo: "samplephotourl",
-    id: "asdnasjdasjkdas",
-};
-getData(productOne);
-//Never Type
-const err = new Error();
-const errorhandler = () => {
-    throw new Error(); // on throw returns never type
-};
-const errorhandler1 = () => {
-    return new Error(); // on return error the type is error
-};
+const mode = 'light';
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,3 @@
-type product = {
-    name: string;
-    stock: number;
-    price: number;
-    photo: string;
-    readonly id : string;
-}
+type thememode = 'light' | 'dark';
 
-type GetDataType = (product :product) => void;
-
-const getData: GetDataType = (product) => {
-    console.log(product);
-};
-    
-const productOne: product = { 
-    name: "Macbook",
-    stock: 46,
-    price: 999999,
-    photo: "samplephotourl",
-    id: "asdnasjdasjkdas",
-};
-
-getData(productOne);
-
-//Never Type
-const err = new Error()
-
-const errorhandler = ()=>{
-    throw new Error(); // on throw returns never type
-}
-
-const errorhandler1 = ()=>{
-    return new Error(); // on return error the type is error
-}
+const mode:thememode = 'light';
