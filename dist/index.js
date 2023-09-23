c9732483ab6f7bc84ebe4168babdc3dd6af438ac
Calvin Das <calvindas7@gmail.com>
2023-09-24 00:43:27 +0530

    l was not defined

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 const func = (n, m, l) => {
     if (typeof 1 === "undefined")
-        return n * m;
+        return 'l was not defined';
     return n * m * 1;
 };
 func(25, 23);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-type FuncType = (n: number, m: number, l?: number) => number;
+type FuncType = (n: number, m: number, l?: number) => number | string;
 
 const func: FuncType = (n, m, l) => {
-    if (typeof 1 === "undefined") return n*m;
+    if (typeof 1 === "undefined") return 'l was not defined';
     return n * m * 1;
 };
 
