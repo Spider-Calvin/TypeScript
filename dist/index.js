498cc31ba70418042eae0ddc66c00611a2c8db02
Calvin Das <calvindas7@gmail.com>
2023-09-24 20:38:13 +0530

    extends in double generic in ts

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,8 @@ const func = (n, o) => {
     return { n, o };
 };
 const ans = func(20, "Lol");
+const funcx = (n, o) => {
+    return { n, o };
+};
+//Extends just extends the type and takes a copy of the original
+const ansX = func(20, 30);
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,11 @@ const func = <T, U>(n:T,o: U):{ n:T, o:U} => {
 }
     
 const ans = func<number, string>(20,"Lol");
+
+    
+const funcx = <X, Y extends X>(n:X,o: Y):{ n:X, o:Y} => {
+    return { n, o }
+}
+//Extends just extends the type and takes a copy of the original
+    
+const ansX = func<number, number>(20,30);
