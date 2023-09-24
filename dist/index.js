e8ce40f571740abf6cc8edb87e12d106a0ebe71d
Calvin Das <calvindas7@gmail.com>
2023-09-24 10:56:35 +0530

    protected in ts class

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,29 @@
 "use strict";
 //classes in typescript
 class player {
-    constructor(height, weight) {
-        this.height = 34;
-        this.weight = 50;
+    constructor(height, weight, age) {
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
         this.myheight = () => {
             return this.height;
         };
         this.height = height;
         this.weight = weight;
+        this.age = age;
     }
 }
-const abhi = new player(100, 150);
+const abhi = new player(100, 150, 20);
 console.log(abhi.myheight());
-// protected keyboard
+console.log(abhi.weight);
+// protected keyboard can only b extended in subclass
+class ProPlayer extends player {
+    constructor(height, weight, age, special) {
+        super(height, weight, age);
+        this.special = special;
+        this.getheight = () => this.age;
+        this.special = special;
+    }
+}
+const spider = new ProPlayer(100, 150, 20, 10);
+console.log(spider.getheight());
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 //classes in typescript
 
 class player {
-    private height = 34;
-    weight = 50;
 
-    constructor(height: number, weight: number){
+    constructor( private height: number, public weight: number, protected age: number ) {
         this.height = height;
         this.weight = weight;
+        this.age = age;
     }
 
     myheight=()=> {
@@ -15,8 +14,22 @@ class player {
 
 }
 
-const abhi = new player(100, 150);
+const abhi = new player(100, 150, 20);
 
-console.log( abhi.myheight())
+console.log( abhi.myheight());
+console.log(abhi.weight);
 
-// protected keyboard
+// protected keyboard can only b extended in subclass
+
+class ProPlayer extends player{
+    constructor( height: number,  weight: number,  age: number , private special: number){
+        super(height, weight, age);
+        this.special =  special;
+    }
+
+    getheight = ()=> this.age;
+}
+
+
+const spider = new ProPlayer(100, 150, 20, 10);
+console.log(spider.getheight());
