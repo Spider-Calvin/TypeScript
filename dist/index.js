bc2af4bc97793639d5a8d60c9d2bad657187b80f
Calvin Das <calvindas7@gmail.com>
2023-09-24 11:21:43 +0530

    all three html type assertions

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,7 @@
 /*
     Type Assertions meaning is "BAAP KO MATH SIKA CHALL"
 */
-const btn = document.getElementById('btn');
+// const btn = document.getElementById("btn")!;
+// const btn = document.getElementById("btn") as HTMLElement;
+const btn = document.getElementById("btn");
 btn.onclick;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@
     Type Assertions meaning is "BAAP KO MATH SIKA CHALL"
 */
 
-const btn =<HTMLElement> document.getElementById('btn');
+// const btn = document.getElementById("btn")!;
+// const btn = document.getElementById("btn") as HTMLElement;
+const btn = <HTMLElement>document.getElementById("btn");
 
 btn.onclick
